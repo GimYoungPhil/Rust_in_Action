a1a82f76d9c016fcce787e1552c4345a569dd155
YoungPhil <youngphil.gim@gmail.com>
2023-02-25 18:14:34 +0900

    fixed

--- ch03/src/ch3-not-quite-file-2.rs
+++ ch03/src/ch3-not-quite-file-2.rs
@@ -4,7 +4,7 @@
 struct File {
     name: String,
     data: Vec<u8>,
-};
+}
 
 fn open(f: &mut File) -> bool {
     true
@@ -40,7 +40,7 @@ fn main() {
 
     let text = String::from_utf8_lossy(&buffer);
 
-    println!(":?", f2);
+    println!("{:?}", f2);
     println!("{} is {} bytes long", &f2.name, f2_length);
     println!("{}", text);
 }
