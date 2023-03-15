f26704b81ce06d522333e7f7d591de0e38187d07
YoungPhilGim <youngphil.gim@gmail.com>
2023-03-15 10:46:08 +0900

    fixed

---
+++ ch05/ex51-int-vs-int/src/main copy 2.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let a: u8 = 0;
+    let b: u8 = 1;
+    let c: u8 = 15;
+    let d: u8 = 127;
+    let e: u8 = 128;
+    let f: u8 = 255;
+    // let g: u8 = 256;
+
+    println!("a: {:08b}", a);
+    println!("b: {:08b}", b);
+    println!("c: {:08b}", c);
+    println!("d: {:08b}", d);
+    println!("e: {:08b}", e);
+    println!("f: {:08b}", f);
+    // println!("g: {:08b}", g);
+}
---
+++ ch05/ex51-int-vs-int/src/main copy 3.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let a: i8 = 0;
+    let b: i8 = 1;
+    let c: i8 = 15;
+    let d: i8 = 127;
+    let e: i8 = -0;
+    let f: i8 = -1;
+    let g: i8 = -2;
+    let h: i8 = -15;
+    let i: i8 = -128;
+
+    println!("a: {:08b}", a);
+    println!("b: {:08b}", b);
+    println!("c: {:08b}", c);
+    println!("d: {:08b}", d);
+    println!("e: {:08b}", e);
+    println!("f: {:08b}", f);
+    println!("g: {:08b}", g);
+    println!("h: {:08b}", h);
+    println!("i: {:08b}", i);
+}
