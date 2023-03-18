6cd8850ad9978a3c69e39d383292e2a41a749da1
YoungPhil <youngphil.gim@gmail.com>
2023-03-18 20:17:20 +0900

    fixed

--- ch05/ex56-endianness/src/main.rs
+++ ch05/ex56-endianness/src/main.rs
@@ -1,10 +1,11 @@
-fn main() {
-    // let base_exponent: 0b_0111_1111; // 127    0
-    // let base_exponent: 0b_1000_0000; // 128    1
-    // let base_exponent: 0b_1000_0001; // 129    2
-    // let base_exponent: 0b_1000_0010; // 130    3
-    // let base_exponent: 0b_1000_0011; // 131    4
-    let base_exponent = 0b_1000_0100; // 132    5
+use std::mem;
 
-    println!("binary: {:b}, dec: {}", base_exponent, base_exponent);
+fn main() {
+    let big_endian: [u16; 1] = [0x0001];
+    let little_endian: [u16; 1] = [0x0100];
+
+    let a: u16 = unsafe { mem::transmute(big_endian) };
+    let b: u16 = unsafe { mem::transmute(little_endian) };
+
+    println!("{} vs {}", a, b);
 }
