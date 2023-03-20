e5b8ce2d77d8659f8dcdc363f433de28e1b89d27
YoungPhil <youngphil.gim@gmail.com>
2023-03-20 18:04:59 +0900

    fixed

--- ch02/ex211/src/main.rs
+++ ch02/ex211/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    let needle = 0o204;
+    let needle = 132;
     let haystack = [1, 1, 2, 5, 15, 52, 132, 203, 877, 4140, 21147];
 
     for item in &haystack {
         if *item == needle {
-            println!("{}", item);
+            println!("match {}", item);
+        } else {
+            println!("no match");
         }
     }
 }
--- ch02/ex28-match-needles/src/main.rs
+++ ch02/ex28-match-needles/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let needle = 42;
-    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+    // let needle = 42;
+    let haystack: [u16; 10] = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
 
     for item in &haystack {
         let result = match item {
@@ -11,5 +11,21 @@ fn main() {
       if result == "hit!" {
           println!("{}: {}", item, result);
       }
+    }
+
+    for item in &haystack {
+        match item {
+            1 => { println!("{}: odd", item); },
+            2 => { println!("{}: even", item); },
+            _ => { println!("{}: some", item); },
+        }
+    }
+
+    for item in &haystack {
+        if *item == 1 {
+            println!(". {}", item);
+        } else {
+            println!("_ {}", item);
+        }
     }
 }
