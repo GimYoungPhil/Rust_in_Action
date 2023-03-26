f9d26929a2357bc5029cff6a1bb0150f276eea83
YoungPhil <youngphil.gim@gmail.com>
2023-03-26 22:12:22 +0900

    fixed

--- ch05/ex514-q/src/main.rs
+++ ch05/ex514-q/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-  println!("Hello, world!");
+  let half: f32 = 0.5;
+
+  println!("{:x}", half.to_bits());
+  // 0011_1111_0000_0000_0000_0000_0000_0000
+
+  let a: u8 = 126;
+  println!("{:b}", a);
 }
